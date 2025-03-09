6b93c1f0ff0d6b62f66c13dacd1182ebc30e9e2b
David Dimande <davidtimoteodimande@gmail.com>
2025-03-09 16:12:40 +0000

    initial cloud init script

---
+++ scripts/template_preparation.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# varibles declaration
+
+download_path="/temp"
+vm_id=8000
+vm_memory_size=2048
+image_url="https://cloud-images.ubuntu.com/oracular/current/"
+image_name="oracular-server-cloudimg-amd64.img"
+storage_name="local-lvm"
+
+
+imageDownload () {
+    # download image file from internet
+    wget $image_url$image_name
+}
+
+createVM () {
+   # create a new VM with VirtIO SCSI controller
+   qm create $vm_id --memory $vm_memory_size --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
+}
+
+importImage () {
+   # import the downloaded disk to the local-lvm storage, attaching it as a SCSI drive
+   
+   #qm set $vm_id --scsi0 local-lvm:0,import-from=/path/to/bionic-server-cloudimg-amd64.img
+   qm set $vm_id --scsi0 $storage_name:0,import-from=$download_path$image_name
+}
+
+insertCD () {
+    # insert image to CD-ROM
+    qm set $vm_id --ide2 $storage_name:cloudinit
+}
+
+setBootOrder () {
+    # set boot order to use image as primary source
+    qm set $vm_id --boot order=scsi0
+}
+
+setConsole () {
+    # configure serial as display device
+    qm set $vm_id --serial0 socket --vga serial0
+}
+
+templateVM () {
+    # convert VM as template
+    qm template $vm_id
+}
