8ab359ee188d77a63b4ae5d7b7a5f5d9e30ad111
David Dimande <davidtimoteodimande@gmail.com>
2025-03-09 19:00:47 +0000

    write logs to syslog service

--- scripts/template_preparation.sh
+++ scripts/template_preparation.sh
@@ -12,58 +12,58 @@ storage_name="local-lvm"
 imageDownload () {
     # download image file from internet
 
-    echo "#### downloading image ...####"
+    echo "#### downloading image ...####" | logger -s -p local7.notice
     wget -nd --tries=5 --timeout=600 --mirror $image_url  -P $download_path
-    echo "#### done! ####"
+    echo "#### done! ####" | logger -s -p local7.notice
 }
 
 createVM () {
    # create a new VM with VirtIO SCSI controller
 
-   echo "#### creating virtual machine ...####"
+   echo "#### creating virtual machine ...####" | logger -s -p local7.notice
    qm create $vm_id --memory $vm_memory_size --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
-   echo "#### done! ####"
+   echo "#### done! ####" | logger -s -p local7.notice 
 }
 
 importImage () {
    # import the downloaded disk to the local-lvm storage, attaching it as a SCSI drive
    #qm set $vm_id --scsi0 local-lvm:0,import-from=/path/to/bionic-server-cloudimg-amd64.img
-   
-   echo "#### importing image file ...####"
+
+   echo "#### importing image file ...####" | logger -s -p local7.notice
    qm set $vm_id --scsi0 $storage_name:0,import-from=$download_path$image_name
-   echo "#### done! ####"
+   echo "#### done! ####" | logger -s -p local7.notice
 }
 
 insertCD () {
     # insert image to CD-ROM
 
-    echo "#### inserting image file to CD-ROM ...####"
+    echo "#### inserting image file to CD-ROM ...####" | logger -s -p local7.notice
     qm set $vm_id --ide2 $storage_name:cloudinit
-    echo "#### done! ####"
+    echo "#### done! ####" | logger -s -p local7.notice
 }
 
 setBootOrder () {
     # set boot order to use image as primary source
 
-    echo "#### setting boot order ...####"
+    echo "#### setting boot order ...####" | logger -s -p local7.notice
     qm set $vm_id --boot order=scsi0
-    echo "#### done! ####"
+    echo "#### done! ####" | logger -s -p local7.notice
 }
 
 setConsole () {
     # configure serial as display device
 
-    echo "#### setting console device ...####"
+    echo "#### setting console device ...####" | logger -s -p local7.notice
     qm set $vm_id --serial0 socket --vga serial0
-    echo "#### done! ####"
+    echo "#### done! ####" | logger -s -p local7.notice
 }
 
 templateVM () {
     # convert VM as template
 
-    echo "#### converting virtual machine as template ...####"
+    echo "#### converting virtual machine as template ...####" | logger -s -p local7.notice
     qm template $vm_id
-    echo "#### done! ####"
+    echo "#### done! ####" | logger -s -p local7.notice
 }
 
 # call functions 
