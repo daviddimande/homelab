437cdb59f1f3ada2a4134e1053f0944c5103f68c
David Dimande <davidtimoteodimande@gmail.com>
2025-03-15 19:01:09 +0000

    formated code

--- scripts/create_terraform_credentials.sh
+++ scripts/create_terraform_credentials.sh
@@ -9,5 +9,5 @@ ROLE=terraformProv
 # create user, role and assign necessary priviledges
 
 pveum role add $ROLE -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use"
-pveum user add $USERNAME --password !!!test!!!2025
+pveum user add $USERNAME --password 'password here' # token id is recommended
 pveum aclmod / -user $USERNAME -role $ROLE
