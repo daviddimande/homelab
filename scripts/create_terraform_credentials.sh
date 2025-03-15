33e492e91de034e8abacbc0ebe864ac26b8f8c12
David Dimande <davidtimoteodimande@gmail.com>
2025-03-15 14:43:33 +0000

    added script to create terraform user and role for automation purpose in future

---
+++ scripts/create_terraform_credentials.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# variables declaration
+
+USERNAME=terraform-prov@pve
+ROLE=terraformProv
+
+# create user, role and assign necessary priviledges
+
+pveum role add $ROLE -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use"
+pveum user add $USERNAME --password !!!test!!!2025
+pveum aclmod / -user $USERNAME -role $ROLE
